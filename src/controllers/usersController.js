053eeb7e53c1fc29ae761c67d14fed0326a40275
Leonardo Otero <leonardo.otero390@gmail.com>
2022-01-22 17:51:35 -0400

    feat: validations on sign-up entries

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,7 +1,16 @@
+import joi from 'joi';
 import users from '../database/users.js';
 
+const imageRegex = /\.(?:png|jpg|jpeg|gif)/;
+
+const userSchema = joi.object({
+  username: joi.string().min(1).required(),
+  avatar: joi.string().uri().pattern(new RegExp(imageRegex)).required(),
+});
 export async function signUp(req, res) {
   const newUser = req.body;
+  const validation = userSchema.validate(newUser);
+  if (validation.error) return res.status(400).send("Todos os campos são obrigatórios!");
   users.push(newUser);
-  res.sendStatus(200);
+  return res.status(201).send("OK");
 }
