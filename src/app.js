0a95bc869173e3565112bb4692c17249a9ad7bf7
Leonardo Otero <leonardo.otero390@gmail.com>
2022-01-22 16:15:05 -0400

    feat: route health working

--- src/app.js
+++ src/app.js
@@ -2,9 +2,10 @@ import express from 'express';
 import cors from 'cors';
 import routes from './routers/routes.js';
 
+
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use(routes());
+app.use(routes);
 
 export default app;
--- src/server.js
+++ src/server.js
@@ -1,9 +1,7 @@
 /* eslint-disable no-console */
 
-import './setup.js';
 import app from './app.js';
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is listening on port ${process.env.PORT}.
-  Server running at ${process.env.NODE_ENV} mode`);
+app.listen(5000, () => {
+  console.log(`Server is listening on port 5000`);
 });
