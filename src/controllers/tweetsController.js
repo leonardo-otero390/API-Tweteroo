e59f0948c552b33637603df3fc4dec849795025d
Leonardo Otero <leonardo.otero390@gmail.com>
2022-01-22 17:16:31 -0400

    refac: route /tweets remove console.log

--- src/controllers/tweetsController.js
+++ src/controllers/tweetsController.js
@@ -3,6 +3,5 @@ import tweets from "../database/tweets.js";
 export async function insertTweet(req, res) {
   const newTweet = req.body;
   tweets.push(newTweet);
-  console.log(tweets);
   res.sendStatus(200);
 }
